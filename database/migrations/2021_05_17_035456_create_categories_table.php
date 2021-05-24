d594d0a98fa2e4d6df043ceb4e2680c7eb2e6e23
namnguyen231 <nguyenphuongnam.tdc2019@gmail.com>
2021-05-24 07:37:12 +0700

    h

--- database/migrations/2021_05_17_035456_create_categories_table.php
+++ database/migrations/2021_05_17_035456_create_categories_table.php
@@ -14,7 +14,7 @@ class CreateCategoriesTable extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->increments('categories');
+            $table->increments('category_id');
             $table->string('category_name', 55);
             $table->timestamps();
         });
